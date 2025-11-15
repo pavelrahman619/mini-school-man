cdd0676b9f566f617a7982f0b9cf90a83a511ac0
pavelrahman619 <pavelrahman.dev@gmail.com>
2025-11-15 19:15:54 +0600

    bug: fix tailwind

--- backend/resources/js/App.vue
+++ backend/resources/js/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <NavigationBar v-if="$route.name !== 'Login'" />
-    <main class="main-content">
+    <main :class="$route.name === 'Login' ? '' : 'main-content'">
       <router-view />
     </main>
   </div>
@@ -18,17 +18,23 @@ import { NavigationBar } from './components';
   box-sizing: border-box;
 }
 
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   min-height: 100vh;
-  background-color: #f5f5f5;
 }
 
 .main-content {
   max-width: 1200px;
   margin: 0 auto;
   padding: 2rem 1rem;
+  background-color: #f5f5f5;
+  min-height: 100vh;
 }
 </style>
